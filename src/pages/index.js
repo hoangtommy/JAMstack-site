e47fb0ad1b21b9d354383a450c2aba1bbe108dd9
Tommy <t@tommyhoang.com>
2018-09-03 18:12:00 -0700

    setup basic files

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hi everyone</h1>
+    <p>Looking forward to FCC Jamstack Hackathon!</p>
+    <p>I am a former public school teacher and most recently worked at the edtech startup, Altschool.</p>
+    <a href="https://tommyhoang.com/" target ="_blank">My website</a>
+  </div>
+)
+
+export default IndexPage
